b9d10a46149305afd517f5c74c66cfe016322517
Alvin <alvingomez.world@gmail.com>
2023-08-05 20:21:26 +0800

    Import and added components
    Added the Container component from React Bootstrap.
    Added the header and footer component to be created later on

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <h1>Welcome to Divisoria</h1>
-  )
-}
+    <>
+      <Header />
+      <main className="py-3">
+        {/*  Containers are used to contain, pad, and (sometimes) 
+           center the content within them. 
+           >>container, which sets a max-width at each responsive breakpoint
+      */}
+        <Container>
+          <h1>Welcome to proshop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
