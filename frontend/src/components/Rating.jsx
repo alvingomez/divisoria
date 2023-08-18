7a25511bd8fbdae4a8005a9e857d3be40d6e5b6d
Alvin <alvingomez.world@gmail.com>
2023-08-18 22:37:52 +0800

    Rating.jsx
    Created Rating component to be used for Homescreen and product page.
    Added manual CSS alignment.

---
+++ frontend/src/components/Rating.jsx
@@ -0,0 +1,28 @@
+import {FaStar, FaStarHalfAlt, FaRegStar} from 'react-icons/fa';
+
+const Rating = ({values, reviews}) => {
+  console.log(values)
+  return (
+    <div className='rating'>
+      <span>
+        {values >= 1 ? <FaStar/> : values >=0.5 ? <FaStarHalfAlt/> : <FaRegStar/>} 
+      </span> 
+      <span>
+        {values >= 2 ? <FaStar/> : values >=1.5 ? <FaStarHalfAlt/> : <FaRegStar/>} 
+      </span>
+      <span>
+        {values >= 3 ? <FaStar/> : values >=2.5 ? <FaStarHalfAlt/> : <FaRegStar/>} 
+      </span>   
+      <span>
+        {values >= 4 ? <FaStar/> : values >=3.5 ? <FaStarHalfAlt/> : <FaRegStar/>} 
+      </span> 
+      <span>
+        {values >= 5 ? <FaStar/> : values >=4.5 ? <FaStarHalfAlt/> : <FaRegStar/>} 
+      </span>    
+
+      <span className="rating-text">{reviews && reviews }</span>       
+    </div>
+  )
+}
+
+export default Rating;
