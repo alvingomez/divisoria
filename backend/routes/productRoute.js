60e7d28a3c4008d00f9d1d7e680478f2a13a8868
Alvin <alvingomez.world@gmail.com>
2023-11-14 22:59:41 +0800

    Router
    Created a separate route for the products and product. This is to make the code look cleaner and a form of refactoring.

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,26 @@
+import express from 'express';
+const router = express.Router();
+
+import asyncHandler from '../middleware/asynchronousHandler.js';
+import Product from '../models/productDB.js'
+
+router.get('/', asyncHandler(async (req, res) => {
+    // FETHCING 'ALL' PRODUCTS FROM THE DATABASE
+    const products = await Product.find({});
+    res.json(products);
+}));
+
+router.get('/:id', asyncHandler(async (req, res) => {
+    //FETCHING A SINGLE PRODUCT
+    const product = await Product.findById(req.param.id)  //This is from the Model API
+
+    // ERROR CATCHER IF PAGE IS NOT AVAILABLE
+    if(product){
+        return res.json(product)
+    } else {
+        res.status(404);
+        throw new Error('Resource not found');
+    }
+}))
+
+export default router;
