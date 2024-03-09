a9fc8e37ef94bd5b06521ca813b453bf0e6d5852
Alvin <alvingomez.world@gmail.com>
2024-03-09 18:26:30 +0800

    InjectEndPoints productAPISlice
    created endpoint o fetch products.

---
+++ frontend/src/slices/productsApiSlice.js
@@ -0,0 +1,14 @@
+import { PRODUCTS_URL } from '../constants';
+import {apiSlice} from '../slices/apiSlice';
+
+const productsApiSlice = apiSlice.injectEndpoints({
+    endpoints: (build) => {
+        getProducts = build.query({
+            query: () => ({url:PRODUCTS_URL,}),
+            keepUnusedDataFor:5,
+        })
+
+    }
+});
+
+export const {useGetProductsquery} = productsApiSlice;
