cdff36c13a632544f73668bb8f1af36b41a144a2
Alvin <alvingomez.world@gmail.com>
2024-03-09 18:29:22 +0800

    Comment
    Added a comment

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -2,13 +2,14 @@ import { PRODUCTS_URL } from '../constants';
 import {apiSlice} from '../slices/apiSlice';
 
 const productsApiSlice = apiSlice.injectEndpoints({
-    endpoints: (build) => {
+    endpoints: (build) => ({
+        // created endpoint for HomeScreen
         getProducts = build.query({
             query: () => ({url:PRODUCTS_URL,}),
             keepUnusedDataFor:5,
         })
 
-    }
+    })
 });
 
 export const {useGetProductsquery} = productsApiSlice;
