c5740fb22165bd63ac1ff3f5bd40aa1e39a8afdf
Alvin <alvingomez.world@gmail.com>
2024-03-13 15:34:21 +0800

    Local state and useEffect
    Removing local state and useEffect and moving to redux state management

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,37 +1,9 @@
-import {useState, useEffect} from 'react';
+
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Products';
 import axios from 'axios';
 
 const HomeScreen = () => {  
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const {data} = await axios.get('/api/products')
-      .catch(function (error) {
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        } else if (error.request) {
-          // The request was made but no response was received
-          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-          // http.ClientRequest in node.js
-          console.log(error.request);
-        } else {
-          // Something happened in setting up the request that triggered an Error
-          console.log('Error', error.message);
-        }
-        console.log(error.config);
-      });
-      setProducts(data); 
-    }
-
-    fetchProducts();
-  }, []);
 
   
   return (
