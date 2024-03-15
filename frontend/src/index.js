3d936341328d8416ed8a2ba9f54d5b3d4262ac83
Alvin <alvingomez.world@gmail.com>
2024-03-15 20:13:38 +0800

    Store and Provider
    Added the Redux store and Provider to share the store to the rest of the components.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,6 +15,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ProductScreen from './screens/ProductScreen';
+import {store} from './store';
+import {Provider} from'react-redux';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -39,7 +41,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
